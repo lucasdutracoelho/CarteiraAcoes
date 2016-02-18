d77c68d12cb5c1cff275bbced09d7124aadc03c2
lucasdutracoelho <lucasdutra@ymail.com>
2016-02-18 18:17:56 -0200

    Inclusão das classes de Teste

--- app/src/main/java/br/com/carteira/acoes/AsyncTask/DownloadXmlTask.java
+++ app/src/main/java/br/com/carteira/acoes/AsyncTask/DownloadXmlTask.java
@@ -1,6 +1,5 @@
 package br.com.carteira.acoes.AsyncTask;
 
-import android.app.Application;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -11,12 +10,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Calendar;
 import java.util.List;
 
+import br.com.carteira.acoes.Downloader.Downloader;
 import br.com.carteira.acoes.Entity.Acao;
 import br.com.carteira.acoes.Parser.AcoesParser;
-import br.com.carteira.acoes.R;
 import de.greenrobot.event.EventBus;
 
 /**
@@ -26,8 +24,10 @@ public class DownloadXmlTask extends AsyncTask<String, Void, List<Acao>> {
 
     private final String TAG = "DownloadXmlTask";
     private Context mContext;
-    public DownloadXmlTask (Context context){
+    private Downloader mDownloader;
+    public DownloadXmlTask (Context context, Downloader downloader){
         mContext = context;
+        mDownloader = downloader;
     }
     @Override
     protected List<Acao> doInBackground(String... urls) {
@@ -42,23 +42,15 @@ public class DownloadXmlTask extends AsyncTask<String, Void, List<Acao>> {
 
     @Override
     protected void onPostExecute(List<Acao> result) {
-
         EventBus.getDefault().post(result);
     }
 
-    // Uploads XML from stackoverflow.com, parses it, and combines it with
-    // HTML markup. Returns HTML string.
     private List<Acao> loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
         InputStream stream = null;
         AcoesParser acaoXmlParser = new AcoesParser();
         List<Acao> entries = null;
-        String title = null;
-        String url = null;
-        String summary = null;
-
-
         try {
-            stream = downloadUrl(urlString);
+            stream = mDownloader.download(urlString);
             entries = acaoXmlParser.parse(stream);
             // Makes sure that the InputStream is closed after the app is
             // finished using it.
@@ -67,36 +59,6 @@ public class DownloadXmlTask extends AsyncTask<String, Void, List<Acao>> {
                 stream.close();
             }
         }
-
-        // StackOverflowXmlParser returns a List (called "entries") of Entry objects.
-        // Each Entry object represents a single post in the XML feed.
-        // This section processes the entries list to combine each entry with HTML markup.
-        // Each entry is displayed in the UI as a link that optionally includes
-        // a text summary.
-        for (Acao entry : entries) {
-            Log.i(TAG, entry.getCodigo());
-        }
         return entries;
-    }
-
-    // Given a string representation of a URL, sets up a connection and gets
-    // an input stream.
-    private InputStream downloadUrl(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setReadTimeout(10000 /* milliseconds */);
-        conn.setConnectTimeout(15000 /* milliseconds */);
-        conn.setRequestMethod("GET");
-        conn.setDoInput(true);
-        // Starts the query
-        conn.connect();
-        InputStream stream = conn.getInputStream();
-        //String xml =  convertStreamToString(stream);
-        return stream;
-    }
-
-    private String convertStreamToString(java.io.InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
     }
 }

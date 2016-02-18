5e32b2159ab5ec05de0ae484a0ec2a6cab6a9f9f
lucasdutracoelho <lucasdutra@ymail.com>
2016-02-18 18:17:49 -0200

    Inclusão das classes de Teste

---
+++ app/src/test/java/br/com/carteira/acoes/DownloadTest.java
@@ -0,0 +1,35 @@
+package br.com.carteira.acoes;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.lang.String;
+
+import br.com.carteira.acoes.Downloader.Downloader;
+import br.com.carteira.acoes.Downloader.DownloaderURL;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class DownloadTest {
+
+    @Test
+    public void DownloadURLValido() throws Exception {
+        String url = "http://www.bmfbovespa.com.br/Pregao-Online/ExecutaAcaoAjax.asp?CodigoPapel=ABEV3|BBPO11";
+        Downloader downloader = new DownloaderURL();
+        InputStream stream = downloader.download(url);
+        Assert.assertNotNull(stream);
+    }
+
+    @Test
+    public void DownloadURLNull() throws Exception {
+        String url = null;
+        Downloader downloader = new DownloaderURL();
+        InputStream stream = downloader.download(url);
+        Assert.assertNull(stream);
+    }
+}
